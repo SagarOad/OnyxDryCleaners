a24e81c16ca31a850b1aaa0960e73ecd6a03e45d
SagarOad <124421949+SagarOad@users.noreply.github.com>
2024-09-05 07:21:59 -0900

    new changes

--- src/components/ReceiptClient.js
+++ src/components/ReceiptClient.js
@@ -25,35 +25,35 @@ const ReceiptClient = ({ data, customerCount, onClose }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div id="receipt" className="receipt-container w-[27rem] rounded bg-white px-6 py-8 shadow-lg">
+      <div id="receipt" className="receipt-container w-[24rem] rounded bg-white px-6 py-8 shadow-lg">
         <div className="flex flex-col items-center mb-4">
           <Image src={logo} alt="Logo" className="w-20 mb-2" />
-          <h4 className="text-xl font-semibold">Onyx Dry Cleaners</h4>
-          <p className="text-[12px] text-gray-600 text-center">
-            Abdullah Sports City Qasimabad, Hyderabad, Sindh
+          <h4 className="text-xl font-semibold text-black">Onyx Dry Cleaners</h4>
+          <p className="text-[12px] text-black text-center">
+            Abdullah Sports Tower Qasimabad, Hyderabad, Sindh
           </p>
         </div>
 
         <div className="border-t border-gray-300 py-4">
           <p className="flex justify-between text-sm mb-2">
-            <span className="font-medium text-left">Customer Count:</span>
-            <span>00{customerCount}</span>
+            <span className="font-medium text-left text-black">Customer Count:</span>
+            <span className=" text-black">00{customerCount}</span>
           </p>
           <p className="flex justify-between text-sm mb-2">
-            <span className="font-medium text-left">Service Type:</span>
-            <span>{data?.service}</span>
+            <span className="font-medium text-left text-black">Service Type:</span>
+            <span className=" text-black">{data?.service}</span>
           </p>
           <p className="flex justify-between text-sm mb-2">
-            <span className="font-medium text-left">Host:</span>
-            <span>Vijay Kumar</span>
+            <span className="font-medium text-left text-black">Host:</span>
+            <span className=" text-black">Vijay Kumar</span>
           </p>
           <p className="flex justify-between text-sm mb-2">
-            <span className="font-medium text-left">Customer:</span>
-            <span>{data?.customer?.name}</span>
+            <span className="font-medium text-left text-black">Customer:</span>
+            <span className=" text-black">{data?.customer?.name}</span>
           </p>
           <p className="flex justify-between text-sm mb-2">
-            <span className="font-medium text-left">Address:</span>
-            <span className=" w-[200px]">{data?.customer?.address}</span>
+            <span className="font-medium text-left text-black">Address:</span>
+            <span className=" w-[200px] text-black">{data?.customer?.address}</span>
           </p>
         </div>
 
@@ -61,68 +61,68 @@ const ReceiptClient = ({ data, customerCount, onClose }) => {
           <table className="w-full text-sm">
             <thead>
               <tr>
-                <th className="text-left text-[12px] py-2">Product</th>
-                <th className="text-left text-[12px] py-2">QTY</th>
-                <th className="text-right text-[12px] py-2">Unit Price</th>
-                <th className="text-right text-[12px] py-2">Total</th>
+                <th className="text-left text-black text-[12px] py-2">Product</th>
+                <th className="text-left text-black text-[12px] py-2">QTY</th>
+                <th className="text-right text-black text-[12px] py-2">Unit Price</th>
+                <th className="text-right text-black text-[12px] py-2">Total</th>
               </tr>
             </thead>
             <tbody>
               {data?.items.length ? (
                 data.items.map((item, index) => (
                   <tr key={index}>
-                    <td className="py-2 text-left">{item.product}</td>
-                    <td className="py-2 text-left">{item.quantity}</td>
-                    <td className="py-2 pl-2 text-right">{item.unitPrice}</td>
-                    <td className="py-2 pl-2 text-right">{item.amount}</td>
+                    <td className="py-2 text-black text-left">{item.product}</td>
+                    <td className="py-2 text-black text-left">{item.quantity}</td>
+                    <td className="py-2 text-black pl-2 text-right">{item.unitPrice}</td>
+                    <td className="py-2 text-black pl-2 text-right">{item.amount}</td>
                   </tr>
                 ))
               ) : (
                 <tr>
-                  <td colSpan="4" className="py-2 text-center">
+                  <td colSpan="4" className="py-2 text-black text-center">
                     No items
                   </td>
                 </tr>
               )}
               {data?.charges?.discount > 0 && (
-                <tr>
-                  <td colSpan="3" className="py-2 font-medium text-right">
+                <tr className=" text-black">
+                  <td colSpan="3" className="py-2 text-black font-medium text-right">
                     Discount
                   </td>
-                  <td className="py-2 text-right">-${(data?.subtotal * (data?.charges?.discount / 100)).toFixed(2)}</td>
+                  <td className="py-2 text-right text-black">-${(data?.subtotal * (data?.charges?.discount / 100)).toFixed(2)}</td>
                 </tr>
               )}
-                <tr className="border-t border-gray-300">
-                <td colSpan="3" className="py-2 pr-2 font-medium text-right">
+                <tr className="border-t border-gray-300 text-black">
+                <td colSpan="3" className="py-2 pr-2 font-medium text-black text-right">
                   Total
                 </td>
-                <td className="py-2 text-right">Rs. {data?.subtotal}</td>
+                <td className="py-2 text-right text-black">Rs. {data?.subtotal}</td>
               </tr>
-              <tr className="border-t border-gray-300">
-                <td colSpan="3" className="py-2 pr-2 font-medium text-right">
+              <tr className="border-t border-gray-300 text-black">
+                <td colSpan="3" className="py-2 pr-2 text-black font-medium text-right">
                   Delivery Charge
                 </td>
-                <td className="py-2 text-right">Rs. {data?.deliveryCharge}</td>
+                <td className="py-2 text-right text-black">Rs. {data?.deliveryCharge}</td>
               </tr>
-              <tr className="border-t border-gray-300">
-                <td colSpan="3" className="py-2 pr-2 font-medium text-right">
+              <tr className="border-t border-gray-300 text-black">
+                <td colSpan="3" className="py-2 pr-2 text-black font-medium text-right">
                   Discount
                 </td>
-                <td className="py-2 text-right">{data?.discount}%</td>
+                <td className="py-2 text-right text-black">{data?.discount}</td>
               </tr>
-              <tr className="border-t border-gray-300">
-                <td colSpan="3" className="py-2 pr-2 font-medium text-right">
-                  Sub Total
+              <tr className="border-t border-gray-300 text-black">
+                <td colSpan="3" className="py-2 pr-2 text-black font-medium text-right">
+                  Grand Total
                 </td>
-                <td className="py-2 text-right">Rs. {totalAmount}</td>
+                <td className="py-2 text-right text-black">Rs. {totalAmount}</td>
               </tr>
             </tbody>
           </table>
         </div>
 
-        <div className="flex justify-between mt-6">
+        <div className="flex justify-between mt-6 text-black">
           <button
-            className="bg-gray-300 px-4 py-2 rounded text-sm text-gray-700"
+            className="bg-gray-300 px-4 py-2 rounded text-sm text-black"
             onClick={onClose}
           >
             Close
